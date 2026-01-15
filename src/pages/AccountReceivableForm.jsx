7ad33f6d9d050efc58211cbe55494877e1d3270b
Diogo Rachid <di.rachid@gmail.com>
2026-01-15 11:16:28 +0000

    File changes

--- src/pages/AccountReceivableForm.jsx
+++ src/pages/AccountReceivableForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { base44 } from '@/api/base44Client';
-import { useQuery, useMutation, useQueryClient } from '@tantml:react-query';
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
 import { toast } from "sonner";
 import { createPageUrl } from '@/utils';
 import { ArrowUpCircle, Loader2, Plus, X } from 'lucide-react';
