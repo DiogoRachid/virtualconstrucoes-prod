32f7a8113c35ee301c4debf1510364f167f0e481
Diogo Rachid <di.rachid@gmail.com>
2026-03-23 14:07:07 +0000

    File changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,6 +14,7 @@ import PortalSelect from './pages/PortalSelect';
 import ColaboradorPortal from './pages/ColaboradorPortal';
 import AdminLogin from './pages/AdminLogin';
 import ColaboradorLogin from './pages/ColaboradorLogin';
+import ProtectedAdminRoute from '@/lib/ProtectedAdminRoute';
 
 const { Pages, Layout, mainPage } = pagesConfig;
 const mainPageKey = mainPage ?? Object.keys(Pages)[0];
