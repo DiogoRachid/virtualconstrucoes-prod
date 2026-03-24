0075e4c2ecbba6c5f81f2e8af028d9b821280d64
Diogo Rachid <di.rachid@gmail.com>
2026-03-24 14:06:07 +0000

    File changes

--- src/pages/ImportInvoiceMapping.jsx
+++ src/pages/ImportInvoiceMapping.jsx
@@ -41,7 +41,7 @@ export default function ImportInvoiceMappingPage() {
     },
     onSuccess: () => {
       queryClient.invalidateQueries({ queryKey: ['invoiceItems', invoiceId] });
-      navigate(createPageUrl('AccountsPayable'));
+      navigate(createPageUrl(`InvoicePayableConfig?invoiceId=${invoiceId}`));
     }
   });
 
